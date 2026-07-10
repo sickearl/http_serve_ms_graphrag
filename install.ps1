7ee779078de78b9f5ec4d108697fd45ea03444f0
sickearl <ferrari.davide1987@gmail.com>
2026-07-10 18:51:36 +0200

    ..

--- install.ps1
+++ install.ps1
@@ -3,8 +3,8 @@ param(
 )
 Write-Host deploy...
 $cwd = $pwd
+
 cp .\config.yaml $Path\config.yaml
-cp .\settings.yaml $Path\settings.yaml
 cp ___venv_Scripts_python_exe_-m_graphrag_cache.json $Path\___venv_Scripts_python_exe_-m_graphrag_cache.json
 cp graphrag_startup.ps1 $Path\graphrag_startup.ps1
 cp 'http serve.ps1' $Path\'http serve.ps1'
@@ -17,6 +17,8 @@ Write-Host creating venv
 python -m venv .venv
 .\.venv\Scripts\python.exe -m pip install -r $cwd\requirements.txt
 npm install
-# .\.venv\Scripts\python.exe -m graphrag init
+.\.venv\Scripts\python.exe -m graphrag init --model . --embedding .
+cp .\settings.yaml $Path\settings.yaml
 rm package.json
+
 cd $cwd
